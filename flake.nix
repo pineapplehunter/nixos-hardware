c24a60861d572e5afedeb36bc703f82a8e85b3e2
Shogo Takata <takata.shogo@riken.jp>
2024-04-24 08:33:10 +0900

    Mouse DAIV Z4-I7I01SR-A

--- flake.nix
+++ flake.nix
@@ -213,6 +213,7 @@
       microsoft-surface-pro-9 = import ./microsoft/surface-pro/9;
       milkv-pioneer = import ./milkv/pioneer;
       morefine-m600 = import ./morefine/m600;
+      mouse-daiv-z4-i7i01sr-a = import ./mouse/daiv/z4-i7i01sr-a;
       msi-b350-tomahawk = import ./msi/b350-tomahawk;
       msi-b550-a-pro = import ./msi/b550-a-pro;
       msi-gs60 = import ./msi/gs60;
